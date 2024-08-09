f73593fcba945daa63809c10734ad8f235a7718b
MER6 <sxsuzbek@gmail.com>
2024-08-09 14:15:29 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+
+try:
+    while True:
+        x, y = pyautogui.position()
+        print(f"X: {x}, Y: {y}")
+        time.sleep(1)  # Har 1 soniyada koordinata yangilanadi
+except K>
+    print("\nKoordinat to'xtatildi(For @Networking_Security).")
